9688bd7ec8997bdd23e1d4b5a984f1685fca8727
eogaiver <eogaiver@hanmail.net>
2023-04-01 11:08:30 +0900

    ver02

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Table } from 'react-bootstrap'
 
 const PostsPage = () => {
+    const [list, setList]=useState([]);
+    const [loading, setLoading] = useState(false)
+    const getPosts = () => {
+        setLoading(true);
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                setList(json);
+                setLoading(false);
+            })
+    }
+
+    //랜더링이 될때마다 실행됨
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    if (loading) return <h1>로딩중입니다...</h1>
     return (
-        <div>
-            <h1>게시글</h1>
-        </div>
+        <Row className='justify-content-center mx-3'>
+            <Col md={10}>
+                <h1 className='text-center my-5'>게시글</h1>
+                <Table striped bordered hever>
+                    <thead>
+                        <tr className='text-center'>
+                            <td>ID.</td>
+                            <td>Title</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {list.map(post=>
+                            <tr key={post.id}>
+                                <td>{post.id}</td>
+                                <td>
+                                    <div className='ellipsis'>
+                                        {post.title}
+                                    </div>
+                                    
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </Col>
+        </Row>
     )
 }
 
